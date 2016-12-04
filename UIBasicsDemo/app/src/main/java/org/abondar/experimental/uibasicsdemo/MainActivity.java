b8cc0947f428bc662c61ab79e84de12fb8e6224f
abondar24 <abondar1992@gmail.com>
2016-12-03 19:46:47 -0800

    menus

--- UIBasicsDemo/app/src/main/java/org/abondar/experimental/uibasicsdemo/MainActivity.java
+++ UIBasicsDemo/app/src/main/java/org/abondar/experimental/uibasicsdemo/MainActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.*;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,11 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ib.setImageResource(R.drawable.icon);
         ib.setOnClickListener(this);
 
-        Button cb = (Button) this.findViewById(R.id.multiControl);
-        cb.setOnClickListener(this);
-
-        Button tb = (Button) this.findViewById(R.id.timeControl);
-        tb.setOnClickListener(this);
+        TextView tv =(TextView)this.findViewById(R.id.cmtv);
+        registerForContextMenu(tv);
 
     }
 
@@ -38,15 +36,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         } else if (view.getId() == R.id.imageButton) {
             Log.v("Image Button", "SAAAALO");
-        } else if (view.getId() == R.id.multiControl) {
-            Intent intent = new Intent(this, MultiControlActivity.class);
-            startActivity(intent);
-        } else if (view.getId() == R.id.timeControl) {
-            Intent intent = new Intent(this, TimeActivity.class);
-            startActivity(intent);
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        int base = Menu.FIRST +100;
+        SubMenu sm = menu.addSubMenu(base,base+1,Menu.NONE,"submenu");
+        sm.add(base,base+2,base+2,"sub menu item1");
+        sm.add(base,base+3,base+3,"sub menu item2");
+
+        return true;
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item1) {
+
+            Intent intent = new Intent(this, MultiControlActivity.class);
+            startActivity(intent);
+        } else if (item.getItemId() == R.id.menu_item2) {
+
+            Intent intent = new Intent(this, TimeActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Sample context menu");
+        menu.add(200,200,200 ,"Action bars");
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Log.v("salo","salo");
+        return true;
+    }
 }
