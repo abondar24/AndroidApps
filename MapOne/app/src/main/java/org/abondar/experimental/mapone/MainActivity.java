9ba4dadff404b89f25a3acce840f7d00e89e5b30
abondar24 <abondar1992@gmail.com>
2016-12-24 16:48:16 -0800

    camera animation, markers,lines

--- MapOne/app/src/main/java/org/abondar/experimental/mapone/MainActivity.java
+++ MapOne/app/src/main/java/org/abondar/experimental/mapone/MainActivity.java
@@ -1,5 +1,6 @@
 package org.abondar.experimental.mapone;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +9,19 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.*;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    GoogleMap map;
-    boolean mapReady = false;
+    private GoogleMap map;
+    private boolean mapReady = false;
+    private boolean newPos = false;
+    private static final int MOVE_MODE = 0;
+    private static final int ANIM_MODE = 1;
+
+    private LatLng moscow = new LatLng(55.7706594, 37.42389249);
+    private LatLng sjc18 = new LatLng(37.40716303, -121.9265720);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +58,53 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        final Button moveButton = (Button) this.findViewById(R.id.move_button);
+        moveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LatLng target;
+                if (!newPos) {
+                    moveCam(moscow, ANIM_MODE, "Cisco Krilatskyi Hills 4");
+                    moveButton.setText(R.string.back_move);
+                    newPos = true;
+                } else {
+                    target = new LatLng(37.40716303, -121.9265720);
+                    moveCam(target, MOVE_MODE, "Cisco Building 18");
+                    moveButton.setText(R.string.default_move);
+                    newPos = false;
+
+                }
+
+            }
+        });
+
+
+        Button triangleButton = (Button) this.findViewById(R.id.triangle_button);
+        triangleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!newPos) {
+
+                    LatLng sjc19 = new LatLng(37.4059630, -121.9273580121);
+                    LatLng sjc17 = new LatLng(37.4083880, -121.9273600);
+
+                    map.addPolyline(new PolylineOptions().geodesic(true)
+                            .add(sjc18)
+                            .add(sjc17)
+                            .add(sjc19)
+                            .add(sjc18));
+
+                } else {
+                   map.addCircle(new CircleOptions()
+                   .center(moscow)
+                   .radius(150)
+                   .strokeColor(Color.BLACK)
+                   .fillColor(Color.argb(64,0,255,0)));
+                }
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -59,8 +112,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mapReady = true;
         map = googleMap;
-        LatLng latLng = new LatLng(37.40716303, -121.9265720);
-        CameraPosition position = CameraPosition.builder().target(latLng).zoom(27).build();
-        map.moveCamera(CameraUpdateFactory.newCameraPosition(position));
+        moveCam(sjc18, 0, "Cisco Building 18");
     }
+
+    private void moveCam(LatLng target, int mode, String markerName) {
+        CameraPosition position = CameraPosition.builder().target(target)
+                .zoom(17)
+                .tilt(75)
+                .build();
+
+        MarkerOptions marker = new MarkerOptions().position(target).title(markerName);
+        map.addMarker(marker);
+
+        switch (mode) {
+            case MOVE_MODE:
+                map.moveCamera(CameraUpdateFactory.newCameraPosition(position));
+                break;
+            case ANIM_MODE:
+                map.animateCamera(CameraUpdateFactory.newCameraPosition(position), 1000, null);
+                break;
+        }
+
+    }
+
 }
